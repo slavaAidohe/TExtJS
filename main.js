51cdcaa53f3005f75710eca0e52e9d3e29b5f2ef
aidar <zefircatitypoigogo@gmail.com>
2022-12-11 21:18:59 +0600

    Initial commit.

---
+++ main.js
@@ -0,0 +1,9 @@
+let selectedRow = null;
+ 
+//Show Alerts
+function showAlert(message, className){
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.creat)
+}
